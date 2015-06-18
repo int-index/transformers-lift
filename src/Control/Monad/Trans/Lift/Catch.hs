81e2f0df7c6654d9f1284cf3635d17e277b20c89
Index Int <vlad.z.4096@gmail.com>
2015-06-18 18:16:21 +0300

    LiftCatch with monad-control

--- src/Control/Monad/Trans/Lift/Catch.hs
+++ src/Control/Monad/Trans/Lift/Catch.hs
@@ -1,1 +1,48 @@
-module Control.Monad.Trans.Lift.Catch where
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DefaultSignatures #-}
+module Control.Monad.Trans.Lift.Catch
+    ( LiftCatch(..)
+    , Catch
+    , module Control.Monad.Trans.Class
+    ) where
+
+import Data.Monoid
+
+import Control.Monad.Signatures
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Control
+
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Identity
+import Control.Monad.Trans.List
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+import qualified Control.Monad.Trans.Cont          as Cont
+import qualified Control.Monad.Trans.RWS.Lazy      as Lazy
+import qualified Control.Monad.Trans.RWS.Strict    as Strict
+import qualified Control.Monad.Trans.State.Lazy    as Lazy
+import qualified Control.Monad.Trans.State.Strict  as Strict
+import qualified Control.Monad.Trans.Writer.Lazy   as Lazy
+import qualified Control.Monad.Trans.Writer.Strict as Strict
+
+class MonadTrans t => LiftCatch t where
+    type CatchStT t a
+    type CatchStT t a = StT t a
+    liftCatch :: Monad m => Catch e m (CatchStT t a) -> Catch e (t m) a
+    default liftCatch
+        :: (MonadTransControl t, CatchStT t a ~ StT t a, Monad m, Monad (t m))
+        => Catch e m (CatchStT t a) -> Catch e (t m) a
+    liftCatch catchE m h = do
+        liftWith (\run -> catchE (run m) (run . h)) >>= restoreT . return
+
+instance LiftCatch (ExceptT e)
+instance LiftCatch IdentityT
+instance LiftCatch ListT
+instance LiftCatch MaybeT
+instance LiftCatch (ReaderT r)
+instance Monoid w => LiftCatch (Lazy.RWST r w s)
+instance Monoid w => LiftCatch (Strict.RWST r w s)
+instance LiftCatch (Lazy.StateT s)
+instance LiftCatch (Strict.StateT s)
+instance Monoid w => LiftCatch (Lazy.WriterT w)
+instance Monoid w => LiftCatch (Strict.WriterT w)
