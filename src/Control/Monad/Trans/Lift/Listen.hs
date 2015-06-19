e644ec56c01f2978a0c21aec1b4ceafd4c0cd7c7
Index Int <vlad.z.4096@gmail.com>
2015-06-19 12:43:49 +0300

    LiftListen and LiftPass

--- src/Control/Monad/Trans/Lift/Listen.hs
+++ src/Control/Monad/Trans/Lift/Listen.hs
@@ -1,1 +1,59 @@
-module Control.Monad.Trans.Lift.Listen where
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+module Control.Monad.Trans.Lift.Listen
+    ( LiftListen(..)
+    , Listen
+    , module Control.Monad.Trans.Class
+    ) where
+
+import Data.Monoid
+
+import Control.Monad.Signatures
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Control
+
+import qualified Control.Monad.Trans.Except        as E
+import qualified Control.Monad.Trans.Identity      as I
+import qualified Control.Monad.Trans.Maybe         as M
+import qualified Control.Monad.Trans.Reader        as R
+import qualified Control.Monad.Trans.Cont          as C
+import qualified Control.Monad.Trans.RWS.Lazy      as RWS.Lazy
+import qualified Control.Monad.Trans.RWS.Strict    as RWS.Strict
+import qualified Control.Monad.Trans.State.Lazy    as S.Lazy
+import qualified Control.Monad.Trans.State.Strict  as S.Strict
+import qualified Control.Monad.Trans.Writer.Lazy   as W.Lazy
+import qualified Control.Monad.Trans.Writer.Strict as W.Strict
+
+class MonadTrans t => LiftListen t where
+    type ListenStT t a
+    type ListenStT t a = StT t a
+    liftListen :: Monad m => Listen w m (ListenStT t a) -> Listen w (t m) a
+    -- TODO: investigate how MonadTransControl can be used for default definition
+
+instance LiftListen (E.ExceptT e) where
+    liftListen = E.liftListen
+
+instance LiftListen I.IdentityT where
+    liftListen = I.mapIdentityT
+
+instance LiftListen M.MaybeT where
+    liftListen = M.liftListen
+
+instance LiftListen (R.ReaderT r) where
+    liftListen = R.mapReaderT
+
+instance LiftListen (S.Lazy.StateT s) where
+    liftListen = S.Lazy.liftListen
+
+instance LiftListen (S.Strict.StateT s) where
+    liftListen = S.Strict.liftListen
+
+instance Monoid w' => LiftListen (W.Lazy.WriterT w') where
+    liftListen listen m = W.Lazy.WriterT $ do
+        ~((a, w'), w) <- listen (W.Lazy.runWriterT m)
+        return ((a, w), w')
+
+instance Monoid w' => LiftListen (W.Strict.WriterT w') where
+    liftListen listen m = W.Strict.WriterT $ do
+        ((a, w'), w) <- listen (W.Strict.runWriterT m)
+        return ((a, w), w')
--- src/Control/Monad/Trans/Lift/Pass.hs
+++ src/Control/Monad/Trans/Lift/Pass.hs
@@ -1,1 +1,59 @@
-module Control.Monad.Trans.Lift.Pass where
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+module Control.Monad.Trans.Lift.Pass
+    ( LiftPass(..)
+    , Pass
+    , module Control.Monad.Trans.Class
+    ) where
+
+import Data.Monoid
+
+import Control.Monad.Signatures
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Control
+
+import qualified Control.Monad.Trans.Except        as E
+import qualified Control.Monad.Trans.Identity      as I
+import qualified Control.Monad.Trans.Maybe         as M
+import qualified Control.Monad.Trans.Reader        as R
+import qualified Control.Monad.Trans.Cont          as C
+import qualified Control.Monad.Trans.RWS.Lazy      as RWS.Lazy
+import qualified Control.Monad.Trans.RWS.Strict    as RWS.Strict
+import qualified Control.Monad.Trans.State.Lazy    as S.Lazy
+import qualified Control.Monad.Trans.State.Strict  as S.Strict
+import qualified Control.Monad.Trans.Writer.Lazy   as W.Lazy
+import qualified Control.Monad.Trans.Writer.Strict as W.Strict
+
+class MonadTrans t => LiftPass t where
+    type PassStT t a
+    type PassStT t a = StT t a
+    liftPass :: Monad m => Pass w m (PassStT t a) -> Pass w (t m) a
+    -- TODO: investigate how MonadTransControl can be used for default definition
+
+instance LiftPass (E.ExceptT e) where
+    liftPass = E.liftPass
+
+instance LiftPass I.IdentityT where
+    liftPass = I.mapIdentityT
+
+instance LiftPass M.MaybeT where
+    liftPass = M.liftPass
+
+instance LiftPass (R.ReaderT r) where
+    liftPass = R.mapReaderT
+
+instance LiftPass (S.Lazy.StateT s) where
+    liftPass = S.Lazy.liftPass
+
+instance LiftPass (S.Strict.StateT s) where
+    liftPass = S.Strict.liftPass
+
+instance Monoid w' => LiftPass (W.Lazy.WriterT w') where
+    liftPass pass m = W.Lazy.WriterT $ pass $ do
+        ~((a, f), w) <- W.Lazy.runWriterT m
+        return ((a, w), f)
+
+instance Monoid w' => LiftPass (W.Strict.WriterT w') where
+    liftPass pass m = W.Strict.WriterT $ pass $ do
+        ((a, f), w) <- W.Strict.runWriterT m
+        return ((a, w), f)
