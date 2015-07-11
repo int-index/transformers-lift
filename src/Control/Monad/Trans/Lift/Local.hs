8e241f133d9889a9aef67174e9f332e4ff8cc806
Index Int <vlad.z.4096@gmail.com>
2015-07-11 19:18:18 +0300

    Do not use mmorph

--- src/Control/Monad/Trans/Lift/Local.hs
+++ src/Control/Monad/Trans/Lift/Local.hs
@@ -4,6 +4,7 @@
 module Control.Monad.Trans.Lift.Local
     ( LiftLocal(..)
     , Local
+    , defaultLiftLocal
     , module Control.Monad.Trans.Class
     ) where
 
@@ -12,20 +13,19 @@ import Data.Monoid
 #endif
 
 import Control.Monad.Trans.Class
-import Control.Monad.Morph
 
-import Control.Monad.Trans.Except
-import Control.Monad.Trans.Identity
-import Control.Monad.Trans.List
-import Control.Monad.Trans.Maybe
-import Control.Monad.Trans.Reader
-import qualified Control.Monad.Trans.Cont          as Cont
-import qualified Control.Monad.Trans.RWS.Lazy      as Lazy
-import qualified Control.Monad.Trans.RWS.Strict    as Strict
-import qualified Control.Monad.Trans.State.Lazy    as Lazy
-import qualified Control.Monad.Trans.State.Strict  as Strict
-import qualified Control.Monad.Trans.Writer.Lazy   as Lazy
-import qualified Control.Monad.Trans.Writer.Strict as Strict
+import qualified Control.Monad.Trans.Cont          as C
+import qualified Control.Monad.Trans.Except        as E
+import qualified Control.Monad.Trans.Identity      as I
+import qualified Control.Monad.Trans.List          as L
+import qualified Control.Monad.Trans.Maybe         as M
+import qualified Control.Monad.Trans.Reader        as R
+import qualified Control.Monad.Trans.RWS.Lazy      as RWS.Lazy
+import qualified Control.Monad.Trans.RWS.Strict    as RWS.Strict
+import qualified Control.Monad.Trans.State.Lazy    as S.Lazy
+import qualified Control.Monad.Trans.State.Strict  as S.Strict
+import qualified Control.Monad.Trans.Writer.Lazy   as W.Lazy
+import qualified Control.Monad.Trans.Writer.Strict as W.Strict
 
 type Local r m a = (r -> r) -> m a -> m a
 
@@ -35,24 +35,48 @@ class MonadTrans t => LiftLocal t where
         => m r
         -> (forall a . Local r    m  a)
         -> (forall a . Local r (t m) a)
-    default liftLocal
-        :: (Monad m, MFunctor t)
-        => m r
-        -> (forall a . Local r    m  a)
-        -> (forall a . Local r (t m) a)
-    liftLocal _ l f = hoist (l f)
 
-instance LiftLocal (Cont.ContT r)
-   where liftLocal a l f = Cont.liftLocal a l f
+defaultLiftLocal
+    :: (Monad m, LiftLocal n)
+    => (forall x . n m x -> t m x)
+    -> (forall o x . t o x -> n o x)
+    -> m r
+    -> (forall a . Local r    m  a)
+    -> (forall a . Local r (t m) a)
+defaultLiftLocal t unT a l f = t . liftLocal a l f . unT
 
-instance LiftLocal (ExceptT e)
-instance LiftLocal IdentityT
-instance LiftLocal ListT
-instance LiftLocal MaybeT
-instance LiftLocal (ReaderT r)
-instance Monoid w => LiftLocal (Lazy.RWST r w s)
-instance Monoid w => LiftLocal (Strict.RWST r w s)
-instance LiftLocal (Lazy.StateT s)
-instance LiftLocal (Strict.StateT s)
-instance Monoid w => LiftLocal (Lazy.WriterT w)
-instance Monoid w => LiftLocal (Strict.WriterT w)
+instance LiftLocal (C.ContT r) where
+    liftLocal a l f = C.liftLocal a l f
+
+instance LiftLocal (E.ExceptT e) where
+    liftLocal _ l f = E.mapExceptT (l f)
+
+instance LiftLocal I.IdentityT where
+    liftLocal _ l f = I.mapIdentityT (l f)
+
+instance LiftLocal L.ListT where
+    liftLocal _ l f = L.mapListT (l f)
+
+instance LiftLocal M.MaybeT where
+    liftLocal _ l f = M.mapMaybeT (l f)
+
+instance LiftLocal (R.ReaderT r) where
+    liftLocal _ l f = R.mapReaderT (l f)
+
+instance Monoid w => LiftLocal (RWS.Lazy.RWST r w s) where
+    liftLocal _ l f = RWS.Lazy.mapRWST (l f)
+
+instance Monoid w => LiftLocal (RWS.Strict.RWST r w s) where
+    liftLocal _ l f = RWS.Strict.mapRWST (l f)
+
+instance LiftLocal (S.Lazy.StateT s) where
+    liftLocal _ l f = S.Lazy.mapStateT (l f)
+
+instance LiftLocal (S.Strict.StateT s) where
+    liftLocal _ l f = S.Strict.mapStateT (l f)
+
+instance Monoid w => LiftLocal (W.Lazy.WriterT w) where
+    liftLocal _ l f = W.Lazy.mapWriterT (l f)
+
+instance Monoid w => LiftLocal (W.Strict.WriterT w) where
+    liftLocal _ l f = W.Strict.mapWriterT (l f)
